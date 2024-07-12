1a5af573d4949fcadef7dbf63623317e63b09104
densavi <d.savitsky93@gmail.com>
2024-07-12 14:13:32 +0300

    added scripts

--- app/js/app.js
+++ app/js/app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   $(".js-menu").on("click", function (e) {
     e.preventDefault();
     $(this).toggleClass("active");
-		$('.mobile-menu').toggleClass('active');
+    $(".mobile-menu").toggleClass("active");
   });
 
   $(window).scroll(function () {
@@ -17,4 +17,46 @@ document.addEventListener("DOMContentLoaded", () => {
       $(".header").removeClass("header__bg");
     }
   });
+
+  const headerHeight = $(".header").outerHeight();
+
+  $(".js-anchor").on("click", function (e) {
+    e.preventDefault();
+    $(".mobile-menu").removeClass("active");
+    $(".header__open-menu").removeClass("active");
+    const targetId = $(this).attr("href");
+    const targetOffset = $(targetId).offset().top - headerHeight;
+
+    $("html, body").stop().animate(
+      {
+        scrollTop: targetOffset,
+      },
+      500
+    );
+
+    updateActiveLink($(this));
+  });
+
+  $(window).on("scroll", function () {
+    const scrollPosition = $(this).scrollTop() + headerHeight + 1;
+
+    $("section[id]").each(function () {
+      const sectionTop = $(this).offset().top;
+      const sectionBottom = sectionTop + $(this).outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        const targetId = $(this).attr("id");
+        const activeAnchor = $(`.js-anchor[href="#${targetId}"]`);
+
+        if (activeAnchor.length) {
+          updateActiveLink(activeAnchor);
+        }
+      }
+    });
+  });
+
+  function updateActiveLink(activeAnchor) {
+    $(".js-anchor").removeClass("active");
+    activeAnchor.addClass("active");
+  }
 });
