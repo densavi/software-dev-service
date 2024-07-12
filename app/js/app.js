d7e1b731c5c77b8846ed3bc7774f1a2e7396ce9e
densavi <d.savitsky93@gmail.com>
2024-07-12 14:06:04 +0300

    header fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,19 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-import $ from 'jQuery';
+import $ from "jQuery";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  $(".js-menu").on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+  });
 
-	$('.js-menu').on('click', function(e) {
-		e.preventDefault();
-		$(this).toggleClass('active');
-	})
-
-})
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 10) {
+      $(".header").addClass("header__bg");
+    } else {
+      $(".header").removeClass("header__bg");
+    }
+  });
+});
